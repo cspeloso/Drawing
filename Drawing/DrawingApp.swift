bb54aae57b95e16c4d8cc07301ef4044c7f0281d
cspeloso <chrispeloso2@gmail.com>
2022-02-20 13:02:29 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Chris Peloso on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
